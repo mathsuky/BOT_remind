349cb5ad27fff0dd71c1a9b686cfd982466f7d89
masky5859 <mathsuky2406@gmail.com>
2024-12-21 22:14:44 +0900

    期日の書き換えコード

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -10,8 +12,10 @@ import (
 	"github.com/joho/godotenv"
 )
 
-// GitHub APIのエンドポイント
-const githubGraphQLEndpoint = "https://api.github.com/graphql"
+const (
+	githubGraphQLEndpoint = "https://api.github.com/graphql"
+	cacheFilePath         = "cache.json"
+)
 
 // ミューテーション用の構造体
 type UpdateProjectV2ItemFieldValueInput struct {
@@ -31,37 +35,163 @@ type Mutation struct {
 	} `graphql:"updateProjectV2ItemFieldValue(input: $input)"`
 }
 
+type GetProjectBaseInfoQuery struct {
+	User struct {
+		ProjectV2 struct {
+			Id    string
+			Items struct {
+				Nodes []struct {
+					Id      string
+					Content struct {
+						Issue struct {
+							Number int
+						} `graphql:"... on Issue"`
+					}
+				}
+			} `graphql:"items(first: 100)"`
+			Fields struct {
+				Nodes []struct {
+					ProjectV2Field struct {
+						Id   string
+						Name string
+					} `graphql:"... on ProjectV2Field"`
+				}
+			} `graphql:"fields(first: 100)"`
+		} `graphql:"projectV2(number: $projectNumber)"`
+	} `graphql:"user(login: $user)"`
+}
+
+type cacheData struct {
+	ID         string
+	IssuesDict map[int]string
+	FieldsDict map[string]graphql.ID
+}
+
+func loadCache() (cacheData, error) {
+	data, err := os.ReadFile(cacheFilePath)
+	if err != nil {
+		return cacheData{}, err
+	}
+
+	var cache cacheData
+	err = json.Unmarshal(data, &cache)
+	if err != nil {
+		return cacheData{}, err
+	}
+
+	return cache, nil
+}
+
+func makeCache(client *graphql.Client) (string, map[int]string, map[string]graphql.ID, error) {
+	var info GetProjectBaseInfoQuery
+	// キャッシュがない場合はクエリを実行してキャッシュを保存
+	err := client.Query(context.Background(), &info, map[string]interface{}{
+		"projectNumber": graphql.Int(3),
+		"user":          graphql.String("mathsuky"),
+	})
+	if err != nil {
+		return "", nil, nil, err
+	}
+
+	projectId := info.User.ProjectV2.Id
+	issuesDict := make(map[int]string)
+	for _, item := range info.User.ProjectV2.Items.Nodes {
+		issuesDict[item.Content.Issue.Number] = item.Id
+	}
+	fieldsDict := make(map[string]graphql.ID)
+	for _, field := range info.User.ProjectV2.Fields.Nodes {
+		fieldsDict[field.ProjectV2Field.Name] = graphql.ID(field.ProjectV2Field.Id)
+	}
+
+	return projectId, issuesDict, fieldsDict, nil
+}
+
+func saveCache(id string, dic1 map[int]string, dic2 map[string]graphql.ID) error {
+	cacheData := cacheData{
+		ID:         id,
+		IssuesDict: dic1,
+		FieldsDict: dic2,
+	}
+
+	os.Remove(cacheFilePath)
+
+	data, err := json.Marshal(cacheData)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(cacheFilePath, data, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("error loading .env file")
 	}
-	// GitHubのPersonal Access Token
-	githubToken := os.Getenv("GITHUB_TOKEN_CLASSIC")
 
-	// HTTPクライアントを作成し、カスタムトランスポートを設定
+	githubToken := os.Getenv("GITHUB_TOKEN_CLASSIC")
 	httpClient := &http.Client{
 		Transport: &transport{
 			token: githubToken,
 		},
 	}
-
-	// GraphQLクライアントを初期化
 	client := graphql.NewClient(githubGraphQLEndpoint, httpClient)
 
-	// ミューテーション用の入力データ
+	// キャッシュから情報を取得
+	baseInfo, err := loadCache()
+	var projectId string
+	var issuesDict map[int]string
+	var fieldsDict map[string]graphql.ID
+	if err != nil {
+		projectId, issuesDict, fieldsDict, err = makeCache(client)
+		if err != nil {
+			log.Fatalf("failed to make cache: %v", err)
+		}
+		err = saveCache(projectId, issuesDict, fieldsDict)
+		if err != nil {
+			log.Fatalf("failed to save cache: %v", err)
+		}
+	}
+
+	// 以下はミューテーションの例
+	var targetIssueId int
+	fmt.Printf("Enter the issue number: ")
+	_, err = fmt.Scanf("%d", &targetIssueId)
+	if err != nil {
+		log.Fatalf("failed to read issue number: %v", err)
+	}
+
+	itemId, ok := baseInfo.IssuesDict[targetIssueId]
+	if !ok {
+		projectId, issuesDict, fieldsDict, err = makeCache(client)
+		if err != nil {
+			log.Fatalf("failed to make cache: %v", err)
+		}
+		err = saveCache(projectId, issuesDict, fieldsDict)
+		if err != nil {
+			log.Fatalf("failed to save cache: %v", err)
+		}
+		itemId, ok = issuesDict[targetIssueId]
+		if !ok {
+			log.Fatalf("issue number %d is not found", targetIssueId)
+		}
+	}
+
 	input := UpdateProjectV2ItemFieldValueInput{
-		ItemID:    "PVTI_lAHOBZSipc4AuISmzgVrKpU",
+		ItemID:    graphql.ID(itemId),
 		ProjectID: "PVT_kwHOBZSipc4AuISm",
 		FieldID:   "PVTF_lAHOBZSipc4AuISmzgkxryw",
 		Value: struct {
 			Date graphql.String `json:"date"`
 		}{
-			Date: graphql.String("2025-05-01"),
+			Date: graphql.String("2025-05-02"),
 		},
 	}
-
-	// ミューテーションを実行
 	var m Mutation
 	log.Printf("Executing mutation with input: %+v\n", input)
 	err = client.Mutate(context.Background(), &m, map[string]interface{}{
