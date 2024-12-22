076d0149fc993f06d8072a7e9fa54d43c3525683
masky5859 <mathsuky2406@gmail.com>
2024-12-22 15:18:25 +0900

    deadlineのセット

--- main.go
+++ main.go
@@ -4,21 +4,85 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 
+	"github.com/hasura/go-graphql-client"
 	"github.com/joho/godotenv"
+	"github.com/mathsuky/BOT_remind/cache"
+	"github.com/mathsuky/BOT_remind/query"
+	"github.com/mathsuky/BOT_remind/transport"
 	traq "github.com/traPtitech/go-traq"
 	traqwsbot "github.com/traPtitech/traq-ws-bot"
 	payload "github.com/traPtitech/traq-ws-bot/payload"
 )
 
-// "github.com/mathsuky/BOT_remind/cache"
-// "github.com/mathsuky/BOT_remind/query"
-// "github.com/mathsuky/BOT_remind/transport"
-// "github.com/hasura/go-graphql-client"
+const githubGraphQLEndpoint = "https://api.github.com/graphql"
 
-// const githubGraphQLEndpoint = "https://api.github.com/graphql"
+func setDeadline(date string, targetIssueId int) error {
+	githubToken := os.Getenv("GITHUB_TOKEN_CLASSIC")
+	httpClient := &http.Client{
+		Transport: &transport.Transport{
+			Token: githubToken,
+		},
+	}
+	client := graphql.NewClient(githubGraphQLEndpoint, httpClient)
+
+	baseInfo, err := cache.LoadCache()
+	var projectId string
+	var issuesDict map[int]string
+	var fieldsDict map[string]graphql.ID
+	if err != nil {
+		projectId, issuesDict, fieldsDict, err = cache.MakeCache(client)
+		if err != nil {
+			return fmt.Errorf("failed to make cache: %v", err)
+		}
+		err = cache.SaveCache(projectId, issuesDict, fieldsDict)
+		if err != nil {
+			return fmt.Errorf("failed to save cache: %v", err)
+		}
+	}
+
+	itemId, ok := baseInfo.IssuesDict[targetIssueId]
+	if !ok {
+		projectId, issuesDict, fieldsDict, err = cache.MakeCache(client)
+		if err != nil {
+			return fmt.Errorf("failed to make cache: %v", err)
+		}
+		err = cache.SaveCache(projectId, issuesDict, fieldsDict)
+		if err != nil {
+			return fmt.Errorf("failed to save cache: %v", err)
+		}
+		itemId, ok = issuesDict[targetIssueId]
+		if !ok {
+			return fmt.Errorf("issue number %d is not found", targetIssueId)
+		}
+	}
+
+	input := query.UpdateProjectV2ItemFieldValueInput{
+		ItemID:    graphql.ID(itemId),
+		ProjectID: "PVT_kwHOBZSipc4AuISm",
+		FieldID:   "PVTF_lAHOBZSipc4AuISmzgkxryw",
+		Value: struct {
+			Date graphql.String `json:"date"`
+		}{
+			Date: graphql.String(date),
+		},
+	}
+	var m query.Mutation
+	log.Printf("Executing mutation with input: %+v\n", input)
+	err = client.Mutate(context.Background(), &m, map[string]interface{}{
+		"input": input,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to execute mutation: %v", err)
+	}
+
+	// 結果を出力
+	log.Printf("Updated project item ID: %s\n", m.UpdateProjectV2ItemFieldValue.ProjectV2Item.ID)
+	return nil
+}
 
 func handleMessage(p *payload.MessageCreated) (string, error) {
 	log.Println("Received MESSAGE_CREATED event: " + p.Message.Text)
@@ -28,15 +92,31 @@ func handleMessage(p *payload.MessageCreated) (string, error) {
 	log.Println(user)
 	log.Println(parts)
 	if len(parts) < 2 {
-		return "Please input valid command.", nil
+		return "有効なコマンドを入力してください。", nil
 	}
 	if parts[1] == "/hello" {
 		return "Hello, world!", nil
 	} else if parts[1] == "/baka" {
 		return "Baka is " + fmt.Sprintf(`!{"type":"user","raw":"@%s","id":"%s"}`, user.Name, user.ID) + "!", nil
-	} else {
-		return "Please input valid command.", nil
+	} else if parts[1] == "/deadline" {
+		// requierd: @BOT_remind deadline <date> <issue number>
+		if len(parts) < 4 {
+			return "十分な引数を提供してください。", nil
+		}
+		date := parts[2]
+		targetIssueId := 0
+		_, err := fmt.Sscanf(parts[3], "%d", &targetIssueId)
+		log.Println(targetIssueId)
+		log.Println(date)
+		if err != nil {
+			return "有効なコマンドを入力してください。", nil
+		}
+		err = setDeadline(date, targetIssueId)
+		if err != nil {
+			return "期日の設定に失敗しました。", err
+		}
 	}
+	return "正しいコマンドを入力してください。", nil
 }
 
 func main() {
@@ -75,74 +155,4 @@ func main() {
 	if err := bot.Start(); err != nil {
 		panic(err)
 	}
-
-	// githubToken := os.Getenv("GITHUB_TOKEN_CLASSIC")
-	// httpClient := &http.Client{
-	// 	Transport: &transport.Transport{
-	// 		Token: githubToken,
-	// 	},
-	// }
-	// client := graphql.NewClient(githubGraphQLEndpoint, httpClient)
-
-	// // キャッシュから情報を取得
-	// baseInfo, err := cache.LoadCache()
-	// var projectId string
-	// var issuesDict map[int]string
-	// var fieldsDict map[string]graphql.ID
-	// if err != nil {
-	// 	projectId, issuesDict, fieldsDict, err = cache.MakeCache(client)
-	// 	if err != nil {
-	// 		log.Fatalf("failed to make cache: %v", err)
-	// 	}
-	// 	err = cache.SaveCache(projectId, issuesDict, fieldsDict)
-	// 	if err != nil {
-	// 		log.Fatalf("failed to save cache: %v", err)
-	// 	}
-	// }
-
-	// // 以下はミューテーションの例
-	// var targetIssueId int
-	// fmt.Printf("Enter the issue number: ")
-	// _, err = fmt.Scanf("%d", &targetIssueId)
-	// if err != nil {
-	// 	log.Fatalf("failed to read issue number: %v", err)
-	// }
-
-	// itemId, ok := baseInfo.IssuesDict[targetIssueId]
-	// if !ok {
-	// 	projectId, issuesDict, fieldsDict, err = cache.MakeCache(client)
-	// 	if err != nil {
-	// 		log.Fatalf("failed to make cache: %v", err)
-	// 	}
-	// 	err = cache.SaveCache(projectId, issuesDict, fieldsDict)
-	// 	if err != nil {
-	// 		log.Fatalf("failed to save cache: %v", err)
-	// 	}
-	// 	itemId, ok = issuesDict[targetIssueId]
-	// 	if !ok {
-	// 		log.Fatalf("issue number %d is not found", targetIssueId)
-	// 	}
-	// }
-
-	// input := query.UpdateProjectV2ItemFieldValueInput{
-	// 	ItemID:    graphql.ID(itemId),
-	// 	ProjectID: "PVT_kwHOBZSipc4AuISm",
-	// 	FieldID:   "PVTF_lAHOBZSipc4AuISmzgkxryw",
-	// 	Value: struct {
-	// 		Date graphql.String `json:"date"`
-	// 	}{
-	// 		Date: graphql.String("2025-05-02"),
-	// 	},
-	// }
-	// var m query.Mutation
-	// log.Printf("Executing mutation with input: %+v\n", input)
-	// err = client.Mutate(context.Background(), &m, map[string]interface{}{
-	// 	"input": input,
-	// })
-	// if err != nil {
-	// 	log.Fatalf("failed to execute mutation: %v", err)
-	// }
-
-	// // 結果を出力
-	// log.Printf("Updated project item ID: %s\n", m.UpdateProjectV2ItemFieldValue.ProjectV2Item.ID)
 }
