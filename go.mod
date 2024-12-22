b50b12690b310160c1d85c783037b5276e1c0c29
masky5859 <mathsuky2406@gmail.com>
2024-12-22 13:58:15 +0900

    traQBOT導入

--- go.mod
+++ go.mod
@@ -7,9 +7,14 @@ toolchain go1.23.4
 require (
 	github.com/hasura/go-graphql-client v0.13.1
 	github.com/joho/godotenv v1.5.1
+	github.com/traPtitech/go-traq v0.0.0-20241109062858-3757c489f610
+	github.com/traPtitech/traq-ws-bot v1.2.1
 )
 
 require (
 	github.com/coder/websocket v1.8.12 // indirect
+	github.com/gofrs/uuid/v5 v5.3.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+	golang.org/x/oauth2 v0.22.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,20 +2,14 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
-	"net/http"
 	"os"
 
-	"github.com/mathsuky/BOT_remind/cache"
-	"github.com/mathsuky/BOT_remind/query"
-	"github.com/mathsuky/BOT_remind/transport"
-
-	"github.com/hasura/go-graphql-client"
 	"github.com/joho/godotenv"
+	traq "github.com/traPtitech/go-traq"
+	traqwsbot "github.com/traPtitech/traq-ws-bot"
+	payload "github.com/traPtitech/traq-ws-bot/payload"
 )
-
-const githubGraphQLEndpoint = "https://api.github.com/graphql"
 
 func main() {
 	err := godotenv.Load()
@@ -23,73 +17,98 @@ func main() {
 		log.Fatalf("error loading .env file")
 	}
 
-	githubToken := os.Getenv("GITHUB_TOKEN_CLASSIC")
-	httpClient := &http.Client{
-		Transport: &transport.Transport{
-			Token: githubToken,
-		},
-	}
-	client := graphql.NewClient(githubGraphQLEndpoint, httpClient)
-
-	// キャッシュから情報を取得
-	baseInfo, err := cache.LoadCache()
-	var projectId string
-	var issuesDict map[int]string
-	var fieldsDict map[string]graphql.ID
-	if err != nil {
-		projectId, issuesDict, fieldsDict, err = cache.MakeCache(client)
-		if err != nil {
-			log.Fatalf("failed to make cache: %v", err)
-		}
-		err = cache.SaveCache(projectId, issuesDict, fieldsDict)
-		if err != nil {
-			log.Fatalf("failed to save cache: %v", err)
-		}
-	}
-
-	// 以下はミューテーションの例
-	var targetIssueId int
-	fmt.Printf("Enter the issue number: ")
-	_, err = fmt.Scanf("%d", &targetIssueId)
-	if err != nil {
-		log.Fatalf("failed to read issue number: %v", err)
-	}
-
-	itemId, ok := baseInfo.IssuesDict[targetIssueId]
-	if !ok {
-		projectId, issuesDict, fieldsDict, err = cache.MakeCache(client)
-		if err != nil {
-			log.Fatalf("failed to make cache: %v", err)
-		}
-		err = cache.SaveCache(projectId, issuesDict, fieldsDict)
-		if err != nil {
-			log.Fatalf("failed to save cache: %v", err)
-		}
-		itemId, ok = issuesDict[targetIssueId]
-		if !ok {
-			log.Fatalf("issue number %d is not found", targetIssueId)
-		}
-	}
-
-	input := query.UpdateProjectV2ItemFieldValueInput{
-		ItemID:    graphql.ID(itemId),
-		ProjectID: "PVT_kwHOBZSipc4AuISm",
-		FieldID:   "PVTF_lAHOBZSipc4AuISmzgkxryw",
-		Value: struct {
-			Date graphql.String `json:"date"`
-		}{
-			Date: graphql.String("2025-05-02"),
-		},
-	}
-	var m query.Mutation
-	log.Printf("Executing mutation with input: %+v\n", input)
-	err = client.Mutate(context.Background(), &m, map[string]interface{}{
-		"input": input,
+	bot, err := traqwsbot.NewBot(&traqwsbot.Options{
+		AccessToken: os.Getenv("TRAQ_BOT_ACCESS_TOKEN"),
 	})
 	if err != nil {
-		log.Fatalf("failed to execute mutation: %v", err)
+		panic(err)
 	}
 
-	// 結果を出力
-	log.Printf("Updated project item ID: %s\n", m.UpdateProjectV2ItemFieldValue.ProjectV2Item.ID)
+	bot.OnMessageCreated(func(p *payload.MessageCreated) {
+		log.Println("Received MESSAGE_CREATED event: " + p.Message.Text)
+		_, _, err := bot.API().
+			MessageApi.
+			PostMessage(context.Background(), p.Message.ChannelID).
+			PostMessageRequest(traq.PostMessageRequest{
+				Content: `!{"type":"user","raw":"@masky5859","id":"1eea935c-0d3c-411b-a565-1b09565237f4"}`,
+			}).
+			Execute()
+		if err != nil {
+			log.Println(err)
+		}
+	})
+
+	if err := bot.Start(); err != nil {
+		panic(err)
+	}
+
+	// githubToken := os.Getenv("GITHUB_TOKEN_CLASSIC")
+	// httpClient := &http.Client{
+	// 	Transport: &transport.Transport{
+	// 		Token: githubToken,
+	// 	},
+	// }
+	// client := graphql.NewClient(githubGraphQLEndpoint, httpClient)
+
+	// // キャッシュから情報を取得
+	// baseInfo, err := cache.LoadCache()
+	// var projectId string
+	// var issuesDict map[int]string
+	// var fieldsDict map[string]graphql.ID
+	// if err != nil {
+	// 	projectId, issuesDict, fieldsDict, err = cache.MakeCache(client)
+	// 	if err != nil {
+	// 		log.Fatalf("failed to make cache: %v", err)
+	// 	}
+	// 	err = cache.SaveCache(projectId, issuesDict, fieldsDict)
+	// 	if err != nil {
+	// 		log.Fatalf("failed to save cache: %v", err)
+	// 	}
+	// }
+
+	// // 以下はミューテーションの例
+	// var targetIssueId int
+	// fmt.Printf("Enter the issue number: ")
+	// _, err = fmt.Scanf("%d", &targetIssueId)
+	// if err != nil {
+	// 	log.Fatalf("failed to read issue number: %v", err)
+	// }
+
+	// itemId, ok := baseInfo.IssuesDict[targetIssueId]
+	// if !ok {
+	// 	projectId, issuesDict, fieldsDict, err = cache.MakeCache(client)
+	// 	if err != nil {
+	// 		log.Fatalf("failed to make cache: %v", err)
+	// 	}
+	// 	err = cache.SaveCache(projectId, issuesDict, fieldsDict)
+	// 	if err != nil {
+	// 		log.Fatalf("failed to save cache: %v", err)
+	// 	}
+	// 	itemId, ok = issuesDict[targetIssueId]
+	// 	if !ok {
+	// 		log.Fatalf("issue number %d is not found", targetIssueId)
+	// 	}
+	// }
+
+	// input := query.UpdateProjectV2ItemFieldValueInput{
+	// 	ItemID:    graphql.ID(itemId),
+	// 	ProjectID: "PVT_kwHOBZSipc4AuISm",
+	// 	FieldID:   "PVTF_lAHOBZSipc4AuISmzgkxryw",
+	// 	Value: struct {
+	// 		Date graphql.String `json:"date"`
+	// 	}{
+	// 		Date: graphql.String("2025-05-02"),
+	// 	},
+	// }
+	// var m query.Mutation
+	// log.Printf("Executing mutation with input: %+v\n", input)
+	// err = client.Mutate(context.Background(), &m, map[string]interface{}{
+	// 	"input": input,
+	// })
+	// if err != nil {
+	// 	log.Fatalf("failed to execute mutation: %v", err)
+	// }
+
+	// // 結果を出力
+	// log.Printf("Updated project item ID: %s\n", m.UpdateProjectV2ItemFieldValue.ProjectV2Item.ID)
 }
