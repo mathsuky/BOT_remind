b2dd40d83797f20027e785dd9ca41586af8f8f3f
masky5859 <mathsuky2406@gmail.com>
2024-12-30 15:11:20 +0900

    選択肢を取得する準備

--- query/querytypes.go
+++ query/querytypes.go
@@ -1,8 +1,6 @@
 package query
 
 import (
-	"context"
-
 	"github.com/hasura/go-graphql-client"
 )
 
@@ -53,35 +51,25 @@ type GetProjectBaseInfoQuery struct {
 			Fields struct {
 				Nodes []struct {
 					ProjectV2Field struct {
-						Id   string
-						Name string
+						Id       string
+						Name     string
+						DataType string `graphql:"dataType"`
 					} `graphql:"... on ProjectV2Field"`
+					ProjectV2SingleSelectField struct {
+						Id      string
+						Name    string
+						Options []struct {
+							Id   string
+							Name string
+						} `graphql:"ProjectV2SingleSelectFieldOption`
+					} `graphql:"... on ProjectV2SingleSelectField"` // これがいるかわからない
 				}
 			} `graphql:"fields(first: 100)"`
 		} `graphql:"projectV2(number: $projectNumber)"`
 	} `graphql:"user(login: $user)"`
 }
 
-func MakeCache(client *graphql.Client) (string, map[int]string, map[string]graphql.ID, error) {
-	var info GetProjectBaseInfoQuery
-	// キャッシュがない場合はクエリを実行してキャッシュを保存
-	err := client.Query(context.Background(), &info, map[string]interface{}{
-		"projectNumber": graphql.Int(3),
-		"user":          graphql.String("mathsuky"),
-	})
-	if err != nil {
-		return "", nil, nil, err
-	}
-
-	projectId := info.User.ProjectV2.Id
-	issuesDict := make(map[int]string)
-	for _, item := range info.User.ProjectV2.Items.Nodes {
-		issuesDict[item.Content.Issue.Number] = item.Id
-	}
-	fieldsDict := make(map[string]graphql.ID)
-	for _, field := range info.User.ProjectV2.Fields.Nodes {
-		fieldsDict[field.ProjectV2Field.Name] = graphql.ID(field.ProjectV2Field.Id)
-	}
-
-	return projectId, issuesDict, fieldsDict, nil
+type ProjectV2SingleSelectFieldOption struct {
+	Id   string `graphql:"id"`
+	Name string `graphql:"name"`
 }
